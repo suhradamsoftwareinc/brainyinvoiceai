82b766c2e9a511de71656ca1dbdc23c27231a33e
meena <meena@localhost>
2024-05-16 12:19:45 +0530

    first commit

--- src/main/java/com/brainyinvoiceai/BrainyinvoiceaiApplication.java
+++ src/main/java/com/brainyinvoiceai/BrainyinvoiceaiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BrainyinvoiceaiApplication {
 
 	public static void main(String[] args) { 
-		SpringApplication.run(BrainyinvoiceaiApplication.class, args);
+		SpringApplication.run(BrainyinvoiceaiApplication.class, args); 
 	}
 
 }
